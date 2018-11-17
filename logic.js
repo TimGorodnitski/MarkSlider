2f7278c5077108e93283e6ad919e677e5f2beb9d
TimG <timgm3@gmail.com>
2018-11-16 19:04:55 -0800

    finished

--- logic.js
+++ logic.js
@@ -6,7 +6,7 @@ var total1 = document.getElementById("total1name");
 var total1value = document.getElementById("total1value");
 output1.innerHTML = slider1.value + "%"; // Display the default slider value
 name1.innerHTML = document.getElementById("dropdown1").value; // Display the default name value
-total1.innerHTML = document.getElementById("dropdown1").value; // Display the default name value
+
 
 var slider2 = document.getElementById("slider2");
 var output2 = document.getElementById("slider2value");
@@ -15,7 +15,6 @@ var total2 = document.getElementById("total2name");
 var total2value = document.getElementById("total2value");
 output2.innerHTML = slider2.value + "%"; // Display the default slider value
 name2.innerHTML = document.getElementById("dropdown2").value; // Display the default name value
-total2.innerHTML = document.getElementById("dropdown2").value; // Display the default name value
 
 var slider3 = document.getElementById("slider3");
 var output3 = document.getElementById("slider3value");
@@ -24,17 +23,15 @@ var total3 = document.getElementById("total3name");
 var total3value = document.getElementById("total3value");
 output3.innerHTML = slider3.value + "%"; // Display the default slider value
 name3.innerHTML = document.getElementById("dropdown3").value; // Display the default name value
-total3.innerHTML = document.getElementById("dropdown3").value; // Display the default name value
 
+var result = document.getElementById("result");
+
+// Update the selection names when a selection is changed
 function updateDropdowns(){
     name1.innerHTML = document.getElementById("dropdown1").value;
-    total1.innerHTML = document.getElementById("dropdown1").value;
     name2.innerHTML = document.getElementById("dropdown2").value;
-    total2.innerHTML = document.getElementById("dropdown2").value;
     name3.innerHTML = document.getElementById("dropdown3").value;
-    total3.innerHTML = document.getElementById("dropdown3").value;
 }
-
 
 // Update the current slider value (each time you drag the slider handle)
 slider1.oninput = function () {
@@ -66,6 +63,16 @@ spent3.oninput = function () {
     spentTotal.innerHTML = (parseInt(spent1.value) + parseInt(spent2.value) + parseInt(spent3.value));
 }
 
+function calculateResult(){
+    var totalWeight = (parseInt(slider1.value) + parseInt(slider2.value) + parseInt(slider3.value));
+
+    console.log("totalWeight: " + totalWeight);
+    
+    result.innerHTML = (
+        (parseInt(slider1.value) / totalWeight * parseInt(spent1.value)) + (parseInt(slider2.value) / totalWeight * parseInt(spent2.value)) + (parseInt(slider3.value) / totalWeight * parseInt(spent3.value))
+    ).toFixed(2)
+
+}
 
 
 
